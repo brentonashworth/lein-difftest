86a550847a25d8843cc6fb11a39a463cc78b5b5f
Brenton Ashworth <bashwort@gmail.com>
2010-07-15 11:50:13 -0700

    Use Robert Hooke to alter behavior of leiningen.test/test. Much simpler implementation.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject lein-difftest "1.1.1"
+(defproject lein-difftest "1.2.0"
   :description "Run tests, display better test results."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojars.brenton/difform "1.0.2"]
-                 [clj-stacktrace "0.1.2"]])
+                 [clj-stacktrace "0.1.2"]
+                 [robert/hooke "1.0.1"]])
---
+++ src/leiningen/hooks/difftest.clj
@@ -0,0 +1,21 @@
+;; Copyright (c) Brenton Ashworth. All rights reserved.
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file COPYING at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by
+;; the terms of this license.
+;; You must not remove this notice, or any other, from this software.
+
+(ns leiningen.hooks.difftest
+  (:require leiningen.test)
+  (:use robert.hooke))
+
+(defn difftest-test-package-hook
+  "Ensure that form-for-testing-namespaces always uses 'difftest.core as the
+   test package."
+  [task & args]
+  (let [args [(first args) 'difftest.core (last args)]]
+    (apply task args)))
+
+(add-hook #'leiningen.test/form-for-testing-namespaces
+          difftest-test-package-hook)

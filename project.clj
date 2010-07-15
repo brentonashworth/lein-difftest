d18b615d51a11ea188fede9a436de5c508e69e6f
Brenton Ashworth <bashwort@gmail.com>
2010-07-15 12:29:21 -0700

    Need hooke as both a regular dependency and dev-dependency in order to work in development and production.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject lein-difftest "1.2.0"
+(defproject lein-difftest "1.2.1"
   :description "Run tests, display better test results."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojars.brenton/difform "1.0.2"]
                  [clj-stacktrace "0.1.2"]
-                 [robert/hooke "1.0.1"]])
+                 [robert/hooke "1.0.1"]]
+  :dev-dependencies [[robert/hooke "1.0.1"]])

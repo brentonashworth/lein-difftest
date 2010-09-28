d1ac2246fcfe8696e09f498fe556f87dc75ffdbd
Brenton Ashworth <bashwort@gmail.com>
2010-09-28 10:39:22 -0700

    With changes in Leiningen 1.3, Robert Hook does not have be a dependency.

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :dependencies [[org.clojure/clojure "1.2.0-RC3"]
                  [org.clojars.brenton/difform "1.1.0"]
                  [clj-stacktrace "0.1.2"]
-                 [clansi "1.0.0"]
-                 [robert/hooke "1.0.1"]]
+                 [clansi "1.0.0"]]
   :dev-dependencies [[robert/hooke "1.0.1"]]
   :hooks [leiningen.hooks.difftest]
   :test-path "test")

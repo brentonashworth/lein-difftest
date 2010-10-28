b189e6ba9e1dea522b6e606c98af9b3a7f0084a1
Phil Hagelberg <technomancy@gmail.com>
2010-10-28 15:17:09 -0700

    Switch from :dev-dependencies to :eval-in-project.

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :dependencies [[org.clojars.brenton/difform "1.1.1"]
                  [clj-stacktrace "0.2.0" :exclusions [org.clojure/clojure]]
                  [clansi "1.0.0" :exclusions [org.clojure/clojure]]]
-  :dev-dependencies [[org.clojure/clojure "1.2.0"]
-                     [org.clojure/clojure-contrib "1.2.0"]]
+  :eval-in-leiningen true
   :hooks [leiningen.hooks.difftest]
   :test-path "test-fail")

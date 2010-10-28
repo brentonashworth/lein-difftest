e9ea8f7e73ae5b763eb00c87ba2410eea358e75e
Phil Hagelberg <technomancy@gmail.com>
2010-10-28 14:15:03 -0700

    Update to latest clj-stacktrace.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject lein-difftest "1.3.2-SNAPSHOT"
   :description "Run tests, display better test results."
   :dependencies [[org.clojars.brenton/difform "1.1.0"]
-                 [clj-stacktrace "0.1.2"]
+                 [clj-stacktrace "0.2.0" :exclusions [org.clojure/clojure]]
                  [clansi "1.0.0"]]
   :hooks [leiningen.hooks.difftest]
   :test-path "test-fail")

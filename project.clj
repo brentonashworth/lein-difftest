eb47eca49b50beb0b1a0e817c67258b19e1f113e
Brenton Ashworth <bashwort@gmail.com>
2010-10-28 14:59:04 -0700

    Update to latest difform. Add clojure 1.2 as a dev dependency.

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject lein-difftest "1.3.2-SNAPSHOT"
   :description "Run tests, display better test results."
-  :dependencies [[org.clojars.brenton/difform "1.1.0"]
+  :dependencies [[org.clojars.brenton/difform "1.1.1"]
                  [clj-stacktrace "0.2.0" :exclusions [org.clojure/clojure]]
-                 [clansi "1.0.0"]]
+                 [clansi "1.0.0" :exclusions [org.clojure/clojure]]]
+  :dev-dependencies [[org.clojure/clojure "1.2.0"]
+                     [org.clojure/clojure-contrib "1.2.0"]]
   :hooks [leiningen.hooks.difftest]
   :test-path "test-fail")

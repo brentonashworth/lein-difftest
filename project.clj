308e579311fb40c857caffc222e8b18ef29736fb
Brenton Ashworth <bashwort@gmail.com>
2011-04-26 20:41:12 -0700

    Change version to 1.3.2

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject lein-difftest "1.3.2-SNAPSHOT"
+(defproject lein-difftest "1.3.2"
   :description "Run tests, display better test results."
   :dependencies [[org.clojars.brenton/difform "1.1.1"]
                  [clj-stacktrace "0.2.0" :exclusions [org.clojure/clojure]]
                  [clansi "1.0.0" :exclusions [org.clojure/clojure]]]
   :eval-in-leiningen true
   :hooks [leiningen.hooks.difftest]
-  :test-path "test-fail")
+  :test-path "test")

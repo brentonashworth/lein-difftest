1f05934f2a133f7bde244a020b0ed75fcc093f19
Brenton Ashworth <bashwort@gmail.com>
2011-05-23 14:59:40 -0400

    Use main difftest project and add deps for clojure and contrib.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject lein-difftest "1.3.2"
+(defproject lein-difftest "1.3.3"
   :description "Run tests, display better test results."
-  :dependencies [[org.clojars.brenton/difform "1.1.1"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [difform "1.1.1"]
                  [clj-stacktrace "0.2.0" :exclusions [org.clojure/clojure]]
                  [clansi "1.0.0" :exclusions [org.clojure/clojure]]]
   :eval-in-leiningen true

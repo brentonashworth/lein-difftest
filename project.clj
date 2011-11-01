fb2431c46b41661155da3d60e8a8d21a5ae2bc87
Phil Hagelberg <technomancy@gmail.com>
2011-11-01 16:08:09 -0700

    Release 1.3.4

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
-(defproject lein-difftest "1.3.4-SNAPSHOT"
+(defproject lein-difftest "1.3.4"
   :description "Run tests, display better test results."
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [difform "1.1.1"]
-                 [clj-stacktrace "0.2.0" :exclusions [org.clojure/clojure]]
-                 [clansi "1.0.0" :exclusions [org.clojure/clojure]]]
+                 [clj-stacktrace "0.2.3"]
+                 [clansi "1.0.0"]]
   :eval-in-leiningen true
-  :hooks [leiningen.hooks.difftest]
-  :test-path "test")
+  :hooks [leiningen.hooks.difftest])

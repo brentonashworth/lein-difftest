8334247814e3587fdbfeedfc148f899bdd35234d
Phil Hagelberg <technomancy@gmail.com>
2012-02-21 22:45:49 -0800

    Release 1.3.8.

--- difftest/project.clj
+++ difftest/project.clj
@@ -1,4 +1,4 @@
-(defproject difftest "1.3.8-SNAPSHOT"
+(defproject difftest "1.3.8"
   :description "Run tests, display better test results."
   :dependencies [[difform "1.1.2"]
                  [clj-stacktrace "0.2.4"]
--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject lein-difftest "1.3.8-SNAPSHOT"
+(defproject lein-difftest "1.3.8"
   :description "Leiningen plugin to improve clojure.test results display."
   :eval-in-leiningen true)
